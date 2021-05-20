1c14d8ad6c64a5115baf2cde724314089a8f3134
sandra <sandravsnair@gmail.com>
2021-05-20 15:06:11 +0530

    Code cleaning

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import sys
 import syntax
 import execute
 import typecheck
 from typecheck import tpcheck
-from execute import flag
 
 for line in syntax.read_lines("> "):
     try:
         if(line == "quit"):
             exit(0)
-        flag = 0
         t = syntax.parse_term(line)
         tp = tpcheck(t)
         if (tp == "Bool" or tp == "Nat"):
--- typecheck.py
+++ typecheck.py
@@ -1,23 +1,20 @@
-import sys
-import os
 import collections
-import re
 import syntax
 
 
 def tpcheck(t):
-    if t=="true" or t=="false":
+    if t == "true" or t == "false":
         return "Bool"
-    elif t=="0" or t[0]=='0':
+    elif t == "0" or t[0] == '0':
         return "Nat"
-    elif t[0]=="succ" or t[0]=="pred":
-        if tpcheck(t[1])=="Nat":
+    elif t[0] == "succ" or t[0] == "pred":
+        if tpcheck(t[1]) == "Nat":
             return "Nat"
         else:
             print("Expected numerical argument to "+t[0])
             return "None"
-    elif t[0]=="iszero":
-        if tpcheck(t[1])=="Nat":
+    elif t[0] == "iszero":
+        if tpcheck(t[1]) == "Nat":
             return "Bool"
         else:
             print("Expected numerical argument to "+t[0])
